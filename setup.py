7a5874cfb8d12bd779e57da500b0cd23b5e2021b
gsand <gsandeepkiran@gmail.com>
2020-10-15 19:25:33 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+"""Setup for the tiger_assessment package."""
+
+import setuptools
+
+
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Sandeep Kiran Gudla",
+    author_email="gsandeepkiran@gmail.com",
+    name='HCSC',
+    license='NA',
+    description='HCSC is a python package for developed as a part of interview process.',
+    version='v0.0.2',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/sandeepkirangudla/HCSC',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=['pandas',
+                      'numpy'],
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+    ],
+)
